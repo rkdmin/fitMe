49c79adf4f3b25d960dd671b1c8000e40f9187cb
22rkdmin <22rkdmin@naver.com>
2023-01-05 22:40:23 +0900

    서비스테스트-주문취소

--- src/test/java/com/zerobase/fitme/order/OrderServiceTest.java
+++ src/test/java/com/zerobase/fitme/order/OrderServiceTest.java
@@ -1,5 +1,7 @@
 package com.zerobase.fitme.order;
 
+import static com.zerobase.fitme.exception.type.OrderErrorCode.ALREADY_START_ITEM;
+import static com.zerobase.fitme.exception.type.OrderErrorCode.ORDER_NOT_FOUND;
 import static com.zerobase.fitme.exception.type.OrderErrorCode.OUT_OF_STOCK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -23,6 +25,7 @@ import com.zerobase.fitme.service.OrderService;
 import com.zerobase.fitme.type.OrderStatus;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -95,5 +98,65 @@ class OrderServiceTest {
         assertEquals(LocalDateTime.now().getMinute(), captor.getValue().getRegDt().getMinute());// 날짜확인
         assertEquals(OrderStatus.준비중, captor.getValue().getOrderStatus());// 준비중인지확인
         assertEquals(cartIdList.get(cartIdList.size() - 1), captor2.getValue());// 장바구니가 잘 삭제됐는지 확인
+    }
+
+    @Test
+    void 주문_취소_실패_주문이없음() {
+        // given
+        Long orderId = 1L;
+
+        given(orderRepository.findById(anyLong()))
+            .willReturn(Optional.empty());
+
+        // when
+        OrderException exception = assertThrows(OrderException.class,
+            () -> orderService.delete(orderId));
+
+        // then
+        assertEquals(ORDER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 주문_취소_실패_배송이시작됨() {
+        // given
+        Long orderId = 1L;
+
+        given(orderRepository.findById(anyLong()))
+            .willReturn(Optional.of(Order.builder()
+                    .orderStatus(OrderStatus.배송완료)// 이미 배송이 완료
+                    .build()));
+
+        // when
+        OrderException exception = assertThrows(OrderException.class,
+            () -> orderService.delete(orderId));
+
+        // then
+        assertEquals(ALREADY_START_ITEM, exception.getErrorCode());
+    }
+
+    @Test
+    void 주문_취소_성공() {
+        // given
+        Long orderId = 1L;
+        int cnt = 10;// 재고 10개(1개 늘어나야됨)
+        Item item = Item.builder()
+            .id(1L)
+            .cnt(cnt)
+            .build();
+        given(orderRepository.findById(anyLong()))
+            .willReturn(Optional.of(Order.builder()
+                    .id(1L)
+                    .item(item)
+                    .orderStatus(OrderStatus.준비중)
+                    .build()));
+
+        ArgumentCaptor<Item> captor = ArgumentCaptor.forClass(Item.class);
+
+        // when
+        orderService.delete(orderId);
+
+        // then
+        verify(itemService, times(1)).update(captor.capture());
+        assertEquals(cnt + 1, captor.getValue().getCnt());// 재고 늘어남
     }
 }
